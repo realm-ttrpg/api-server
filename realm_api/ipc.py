a8388e161a50e3ffafec4258a0f93859d7ae3a49
haliphax <haliphax@gmail.com>
2025-03-07 12:06:30 -0600

    ✨ configurable REDIS_HOST

--- realm_api/ipc.py
+++ realm_api/ipc.py
@@ -1,7 +1,10 @@
+# stdlib
+import os
+
 # 3rd party
 import redis
 
-redis_conn = redis.StrictRedis()
+redis_conn = redis.StrictRedis(host=os.environ.get("REDIS_HOST", "localhost"))
 pubsub = redis_conn.pubsub(ignore_subscribe_messages=True)
 
 
