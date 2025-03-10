bb03cbdc102495a5d65c0bb151514cd596ec20d2
haliphax <haliphax@gmail.com>
2025-03-10 15:03:44 -0500

    🗃️ max_length for UserSession.realm_token

--- realm_api/models/user_session.py
+++ realm_api/models/user_session.py
@@ -10,4 +10,4 @@ from sqlmodel import Field, SQLModel
 class UserSession(SQLModel, table=True):
     user_id: str = Field(primary_key=True, max_length=32)
     discord_token: str = Field(max_length=64)
-    realm_token: str = Field(default_factory=uuid4, index=True)
+    realm_token: str = Field(default_factory=uuid4, index=True, max_length=36)
