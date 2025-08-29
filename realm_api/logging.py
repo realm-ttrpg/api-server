5e7797edd1546f34174cea8ede6b22e1b443d0b9
haliphax <haliphax@gmail.com>
2025-08-29 15:21:36 -0500

    🔊 colorlog

--- realm_api/logging.py
+++ realm_api/logging.py
@@ -4,9 +4,20 @@
 from logging import getLogger, StreamHandler
 from os import environ
 
+# 3rd party
+import colorlog
+
 
 logger = getLogger()
 """Root logger"""
 
-logger.addHandler(StreamHandler())
+streamHandler = StreamHandler()
+streamHandler.setFormatter(
+    colorlog.ColoredFormatter(
+        "{asctime} {log_color}{levelname:<7}{reset} "
+        "{bold_white}{module}:{funcName}{reset} {cyan}\u00bb{reset} {message}",
+        style="{",
+    )
+)
+logger.addHandler(streamHandler)
 logger.setLevel(environ.get("LOG_LEVEL", "INFO"))
